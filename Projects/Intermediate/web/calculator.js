da16dc36733abb9305ebd2e782fe16cadd059341
GlaryCZ <kmatl@email.cz>
2023-06-24 14:19:58 +0200

    calculator works

--- Projects/Intermediate/web/calculator.js
+++ Projects/Intermediate/web/calculator.js
@@ -1,4 +1,5 @@
 var selectedValues = []; // Pole pro ukládání hodnot z tlačítek
+const operators = ["*", "/", "+", "-"]
 
 function storeValue(button) {
     var value = button.innerHTML;
@@ -31,15 +32,26 @@ function getPriklad() {
 function getVysledek(negr) {
     negr = negr.replace(" = ", "")
     // najdi nasobeni
-    while (negr.includes("*")) {
-        var cislo = 0
-        var cisla = []
-        r1 = /[0-9]+[.]?[0-9]*[*][0-9]+[.]?[0-9]*/g
-        var results = negr.match(r1);
-        for (let i = 0; i < results.length; i++) {
-            cisla = results[i].split("*")
-            cislo = parseFloat(cisla[0]) * parseFloat(cisla[1])
-            negr = negr.replace(results[i], cislo.toString())
+    for (i = 0; i < operators.length; i++) {
+        while (negr.includes(operators[i])) {
+            var cislo = 0
+            var cisla = []
+            // r1 = /[0-9]+[.]?[0-9]*[*][0-9]+[.]?[0-9]*/g
+            var r1 = new RegExp(`[0-9]+[.]?[0-9]*[${operators[i]}][0-9]+[.]?[0-9]*`);
+            var results = negr.match(r1);
+            for (let j = 0; j < results.length; j++) {
+                cisla = results[j].split(operators[i])
+                if (operators[i] == "*") {
+                    cislo = parseFloat(cisla[0]) * parseFloat(cisla[1])
+                } else if (operators[i] == "/") {
+                    cislo = parseFloat(cisla[0]) / parseFloat(cisla[1])
+                } else if (operators[i] == "+") {
+                    cislo = parseFloat(cisla[0]) + parseFloat(cisla[1])
+                } else if (operators[i] == "-") {
+                    cislo = parseFloat(cisla[0]) - parseFloat(cisla[1])
+                } else { alert("CRITICAL ERROR") }
+                negr = negr.replace(results[j], cislo.toString())
+            }
         }
     }
     document.getElementById("vysledek").innerHTML = negr;
