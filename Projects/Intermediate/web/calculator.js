8a1b05903353e073263bf06c88aeff077fe24c32
GlaryCZ <kmatl@email.cz>
2023-06-25 21:31:11 +0200

    I gave up calculator

--- Projects/Intermediate/web/calculator.js
+++ Projects/Intermediate/web/calculator.js
@@ -31,28 +31,29 @@ function getPriklad() {
 
 function getVysledek(negr) {
     negr = negr.replace(" = ", "")
-    // najdi nasobeni
-    for (i = 0; i < operators.length; i++) {
-        while (negr.includes(operators[i])) {
+    for (var i = 0; i < operators.length; i++) {
+        while (negr.includes(operators[i]) && (negr.charAt(0) != "-")) {
             var cislo = 0
             var cisla = []
-            // r1 = /[0-9]+[.]?[0-9]*[*][0-9]+[.]?[0-9]*/g
             var r1 = new RegExp(`[0-9]+[.]?[0-9]*[${operators[i]}][0-9]+[.]?[0-9]*`);
-            var results = negr.match(r1);
-            for (let j = 0; j < results.length; j++) {
-                cisla = results[j].split(operators[i])
+            var results = negr.match(r1); // 5+59*5
+            for (let j = 0; j < results.length; j++) { // 59*5
+                cisla = results[j].split(operators[i]) //[59, 5]
                 if (operators[i] == "*") {
-                    cislo = parseFloat(cisla[0]) * parseFloat(cisla[1])
+                    cislo = (parseFloat(cisla[0])) * (parseFloat(cisla[1]))
                 } else if (operators[i] == "/") {
-                    cislo = parseFloat(cisla[0]) / parseFloat(cisla[1])
+                    cislo = (parseFloat(cisla[0])) / (parseFloat(cisla[1]))
                 } else if (operators[i] == "+") {
-                    cislo = parseFloat(cisla[0]) + parseFloat(cisla[1])
+                    cislo = (parseFloat(cisla[0])) + (parseFloat(cisla[1]))
                 } else if (operators[i] == "-") {
-                    cislo = parseFloat(cisla[0]) - parseFloat(cisla[1])
+                    cislo = (parseFloat(cisla[0])) - (parseFloat(cisla[1]))
                 } else { alert("CRITICAL ERROR") }
-                negr = negr.replace(results[j], cislo.toString())
+                negr = negr.replace(results[j], cislo.toString()) // 300
             }
         }
     }
-    document.getElementById("vysledek").innerHTML = negr;
+
+
+    document.getElementById("vysledek").innerHTML = negr
+
 }
